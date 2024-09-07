556a1edc99f081e187ef129c8c9d1097dc9aa57c
BelenBencardino <belenbencardino@hotmail.com>
2024-09-07 12:41:54 -0300

    Constructor de la clase Persona

--- TemplateTPIntegrador/Datos/Persona/Persona.cs
+++ TemplateTPIntegrador/Datos/Persona/Persona.cs
@@ -11,9 +11,22 @@ namespace Datos.Persona
         public string nombre { get; set; }
         public string apellido { get; set; }
         public string email { get; set; }
-        public Guid ID { get; set; }    
+        public Guid id { get; set; }    
         public DateTime fechaalta { get; set; }
-        public DateTime fechabaja { get; set; } 
+        public DateTime fechabaja { get; set; }
 
+        //constructor//
+        public Persona(string nombre, string apellido, string email,Guid id, DateTime fechaalta, DateTime fechabaja)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.email = email;
+            this.id = id;
+            this.fechaalta = DateTime.Now;
+            this.fechabaja = fechabaja;
+
+        }
     }
+
+
 }
