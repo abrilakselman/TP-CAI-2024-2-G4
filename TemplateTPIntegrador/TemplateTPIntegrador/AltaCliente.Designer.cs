09018ca3e7fa9f3f895879a82fb32ff2ad70c4b6
abrilakselman <68AK42663121@campus.economicas.uba.ar>
2024-09-17 19:44:39 -0300

    Formulario Alta de Cliente

---
+++ TemplateTPIntegrador/TemplateTPIntegrador/AltaCliente.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace TemplateTPIntegrador
+{
+    partial class AltaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TituloAltaCliente = new System.Windows.Forms.Label();
+            this.NombreCliente = new System.Windows.Forms.Label();
+            this.TituloApellidoCliente = new System.Windows.Forms.Label();
+            this.TituloDireccionCliente = new System.Windows.Forms.Label();
+            this.TituloTelefonoCliente = new System.Windows.Forms.Label();
+            this.TituloEmailCliente = new System.Windows.Forms.Label();
+            this.Titulonacimientocliente = new System.Windows.Forms.Label();
+            this.TituloDNICliente = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonAceptarCliente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // TituloAltaCliente
+            // 
+            this.TituloAltaCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.TituloAltaCliente.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TituloAltaCliente.ForeColor = System.Drawing.Color.White;
+            this.TituloAltaCliente.Location = new System.Drawing.Point(6, 20);
+            this.TituloAltaCliente.Name = "TituloAltaCliente";
+            this.TituloAltaCliente.Size = new System.Drawing.Size(682, 40);
+            this.TituloAltaCliente.TabIndex = 1;
+            this.TituloAltaCliente.Text = "Alta de Cliente";
+            this.TituloAltaCliente.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NombreCliente
+            // 
+            this.NombreCliente.AutoSize = true;
+            this.NombreCliente.Location = new System.Drawing.Point(12, 94);
+            this.NombreCliente.Name = "NombreCliente";
+            this.NombreCliente.Size = new System.Drawing.Size(44, 13);
+            this.NombreCliente.TabIndex = 2;
+            this.NombreCliente.Text = "Nombre";
+            this.NombreCliente.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // TituloApellidoCliente
+            // 
+            this.TituloApellidoCliente.AutoSize = true;
+            this.TituloApellidoCliente.Location = new System.Drawing.Point(349, 94);
+            this.TituloApellidoCliente.Name = "TituloApellidoCliente";
+            this.TituloApellidoCliente.Size = new System.Drawing.Size(44, 13);
+            this.TituloApellidoCliente.TabIndex = 3;
+            this.TituloApellidoCliente.Text = "Apellido";
+            this.TituloApellidoCliente.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // TituloDireccionCliente
+            // 
+            this.TituloDireccionCliente.AutoSize = true;
+            this.TituloDireccionCliente.Location = new System.Drawing.Point(12, 277);
+            this.TituloDireccionCliente.Name = "TituloDireccionCliente";
+            this.TituloDireccionCliente.Size = new System.Drawing.Size(52, 13);
+            this.TituloDireccionCliente.TabIndex = 4;
+            this.TituloDireccionCliente.Text = "Dirección";
+            // 
+            // TituloTelefonoCliente
+            // 
+            this.TituloTelefonoCliente.AutoSize = true;
+            this.TituloTelefonoCliente.Location = new System.Drawing.Point(12, 150);
+            this.TituloTelefonoCliente.Name = "TituloTelefonoCliente";
+            this.TituloTelefonoCliente.Size = new System.Drawing.Size(49, 13);
+            this.TituloTelefonoCliente.TabIndex = 5;
+            this.TituloTelefonoCliente.Text = "Teléfono";
+            // 
+            // TituloEmailCliente
+            // 
+            this.TituloEmailCliente.AutoSize = true;
+            this.TituloEmailCliente.Location = new System.Drawing.Point(349, 150);
+            this.TituloEmailCliente.Name = "TituloEmailCliente";
+            this.TituloEmailCliente.Size = new System.Drawing.Size(32, 13);
+            this.TituloEmailCliente.TabIndex = 6;
+            this.TituloEmailCliente.Text = "Email";
+            // 
+            // Titulonacimientocliente
+            // 
+            this.Titulonacimientocliente.AutoSize = true;
+            this.Titulonacimientocliente.Location = new System.Drawing.Point(349, 212);
+            this.Titulonacimientocliente.Name = "Titulonacimientocliente";
+            this.Titulonacimientocliente.Size = new System.Drawing.Size(108, 13);
+            this.Titulonacimientocliente.TabIndex = 7;
+            this.Titulonacimientocliente.Text = "Fecha de Nacimiento";
+            // 
+            // TituloDNICliente
+            // 
+            this.TituloDNICliente.AutoSize = true;
+            this.TituloDNICliente.Location = new System.Drawing.Point(12, 212);
+            this.TituloDNICliente.Name = "TituloDNICliente";
+            this.TituloDNICliente.Size = new System.Drawing.Size(26, 13);
+            this.TituloDNICliente.TabIndex = 8;
+            this.TituloDNICliente.Text = "DNI";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(352, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 166);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(231, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 293);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(231, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(352, 166);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(221, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(352, 228);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(221, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // buttonAceptarCliente
+            // 
+            this.buttonAceptarCliente.BackColor = System.Drawing.Color.Navy;
+            this.buttonAceptarCliente.ForeColor = System.Drawing.Color.White;
+            this.buttonAceptarCliente.Location = new System.Drawing.Point(569, 406);
+            this.buttonAceptarCliente.Name = "buttonAceptarCliente";
+            this.buttonAceptarCliente.Size = new System.Drawing.Size(104, 32);
+            this.buttonAceptarCliente.TabIndex = 15;
+            this.buttonAceptarCliente.Text = "Aceptar";
+            this.buttonAceptarCliente.UseVisualStyleBackColor = false;
+            this.buttonAceptarCliente.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AltaUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAceptarCliente);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TituloDNICliente);
+            this.Controls.Add(this.Titulonacimientocliente);
+            this.Controls.Add(this.TituloEmailCliente);
+            this.Controls.Add(this.TituloTelefonoCliente);
+            this.Controls.Add(this.TituloDireccionCliente);
+            this.Controls.Add(this.TituloApellidoCliente);
+            this.Controls.Add(this.NombreCliente);
+            this.Controls.Add(this.TituloAltaCliente);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AltaUsuario";
+            this.Text = "AltaUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TituloAltaCliente;
+        private System.Windows.Forms.Label NombreCliente;
+        private System.Windows.Forms.Label TituloApellidoCliente;
+        private System.Windows.Forms.Label TituloDireccionCliente;
+        private System.Windows.Forms.Label TituloTelefonoCliente;
+        private System.Windows.Forms.Label TituloEmailCliente;
+        private System.Windows.Forms.Label Titulonacimientocliente;
+        private System.Windows.Forms.Label TituloDNICliente;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonAceptarCliente;
+    }
+}
