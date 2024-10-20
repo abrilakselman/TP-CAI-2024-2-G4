68d80f79e2cf3292326bbd0106ff87301c3070ff
myrnav <111702860+myrnav@users.noreply.github.com>
2024-10-20 20:57:07 -0300

    nuevo test

--- TemplateTPIntegrador/Datos/Producto/Producto.cs
+++ TemplateTPIntegrador/Datos/Producto/Producto.cs
@@ -11,13 +11,13 @@ namespace Datos.Producto
         //aclaracion: todos los id por ahora, los pongo con GUID (a chequear)
         public string nombre {  get; set; }
         public Guid id { get; set; }
-
         public Guid idcategoria { get; set; }
         public DateTime fechaalta { get; set; }
         public DateTime fechabaja { get; set; }
         public double precio { get; set; }
         public int stock { get; set; }
         public Guid idusuario { get; set; }
+
 
         public Guid idproveedor { get; set; }
 
