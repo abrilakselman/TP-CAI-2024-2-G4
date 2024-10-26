609d2c00cf5f5ac75f61479cc92a1c5aef035995
abrilakselman <68AK42663121@campus.economicas.uba.ar>
2024-10-25 21:07:37 -0300

    Hice el form de reporte de gestion -Vendido por categoria

---
+++ TemplateTPIntegrador/TemplateTPIntegrador/FormReporteVenidoPorCategoria.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TemplateTPIntegrador
+{
+    partial class FormReporteVenidoPorCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonGenerarPvxC = new System.Windows.Forms.Button();
+            this.ButtonVolverPVxC = new System.Windows.Forms.Button();
+            this.LabelProdVendxCat = new System.Windows.Forms.Label();
+            this.cmbPVxC = new System.Windows.Forms.ComboBox();
+            this.dgvPVxC = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPVxC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonGenerarPvxC
+            // 
+            this.ButtonGenerarPvxC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ButtonGenerarPvxC.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonGenerarPvxC.Location = new System.Drawing.Point(451, 68);
+            this.ButtonGenerarPvxC.Name = "ButtonGenerarPvxC";
+            this.ButtonGenerarPvxC.Size = new System.Drawing.Size(133, 37);
+            this.ButtonGenerarPvxC.TabIndex = 2;
+            this.ButtonGenerarPvxC.Text = "Generar";
+            this.ButtonGenerarPvxC.UseVisualStyleBackColor = false;
+            // 
+            // ButtonVolverPVxC
+            // 
+            this.ButtonVolverPVxC.BackColor = System.Drawing.Color.Blue;
+            this.ButtonVolverPVxC.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonVolverPVxC.ForeColor = System.Drawing.SystemColors.Control;
+            this.ButtonVolverPVxC.Location = new System.Drawing.Point(636, 401);
+            this.ButtonVolverPVxC.Name = "ButtonVolverPVxC";
+            this.ButtonVolverPVxC.Size = new System.Drawing.Size(133, 37);
+            this.ButtonVolverPVxC.TabIndex = 3;
+            this.ButtonVolverPVxC.Text = "Volver";
+            this.ButtonVolverPVxC.UseVisualStyleBackColor = false;
+            // 
+            // LabelProdVendxCat
+            // 
+            this.LabelProdVendxCat.AutoSize = true;
+            this.LabelProdVendxCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelProdVendxCat.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LabelProdVendxCat.Location = new System.Drawing.Point(48, 9);
+            this.LabelProdVendxCat.Name = "LabelProdVendxCat";
+            this.LabelProdVendxCat.Size = new System.Drawing.Size(721, 31);
+            this.LabelProdVendxCat.TabIndex = 4;
+            this.LabelProdVendxCat.Text = "Reporte de Gestion -Productos vendidos por categoria";
+            // 
+            // cmbPVxC
+            // 
+            this.cmbPVxC.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.cmbPVxC.FormattingEnabled = true;
+            this.cmbPVxC.Location = new System.Drawing.Point(230, 76);
+            this.cmbPVxC.Name = "cmbPVxC";
+            this.cmbPVxC.Size = new System.Drawing.Size(195, 21);
+            this.cmbPVxC.TabIndex = 5;
+            // 
+            // dgvPVxC
+            // 
+            this.dgvPVxC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPVxC.Location = new System.Drawing.Point(65, 131);
+            this.dgvPVxC.Name = "dgvPVxC";
+            this.dgvPVxC.Size = new System.Drawing.Size(661, 238);
+            this.dgvPVxC.TabIndex = 6;
+            // 
+            // FormReporteVenidoPorCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvPVxC);
+            this.Controls.Add(this.cmbPVxC);
+            this.Controls.Add(this.LabelProdVendxCat);
+            this.Controls.Add(this.ButtonVolverPVxC);
+            this.Controls.Add(this.ButtonGenerarPvxC);
+            this.Name = "FormReporteVenidoPorCategoria";
+            this.Text = "FormReporteVenidoPorCategoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPVxC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonGenerarPvxC;
+        private System.Windows.Forms.Button ButtonVolverPVxC;
+        private System.Windows.Forms.Label LabelProdVendxCat;
+        private System.Windows.Forms.ComboBox cmbPVxC;
+        private System.Windows.Forms.DataGridView dgvPVxC;
+    }
+}
--- TemplateTPIntegrador/TemplateTPIntegrador/FormVentasPorVendedor.Designer.cs
+++ TemplateTPIntegrador/TemplateTPIntegrador/FormVentasPorVendedor.Designer.cs
@@ -28,54 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.ButtonGenerar = new System.Windows.Forms.Button();
-            this.ButtonVolver = new System.Windows.Forms.Button();
+            this.labelVentasxVendedor = new System.Windows.Forms.Label();
+            this.ButtonGenerarVxV = new System.Windows.Forms.Button();
+            this.ButtonVolverVxV = new System.Windows.Forms.Button();
             this.cmbVentasxVendedor = new System.Windows.Forms.ComboBox();
             this.dgvVentarxVendor = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgvVentarxVendor)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // labelVentasxVendedor
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(108, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(555, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Reporte de Gestion -Ventas por vendedor";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.labelVentasxVendedor.AutoSize = true;
+            this.labelVentasxVendedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVentasxVendedor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelVentasxVendedor.Location = new System.Drawing.Point(108, 18);
+            this.labelVentasxVendedor.Name = "labelVentasxVendedor";
+            this.labelVentasxVendedor.Size = new System.Drawing.Size(555, 31);
+            this.labelVentasxVendedor.TabIndex = 0;
+            this.labelVentasxVendedor.Text = "Reporte de Gestion -Ventas por vendedor";
+            this.labelVentasxVendedor.Click += new System.EventHandler(this.label1_Click);
             // 
-            // ButtonGenerar
+            // ButtonGenerarVxV
             // 
-            this.ButtonGenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ButtonGenerar.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonGenerar.Location = new System.Drawing.Point(233, 76);
-            this.ButtonGenerar.Name = "ButtonGenerar";
-            this.ButtonGenerar.Size = new System.Drawing.Size(133, 37);
-            this.ButtonGenerar.TabIndex = 1;
-            this.ButtonGenerar.Text = "Generar";
-            this.ButtonGenerar.UseVisualStyleBackColor = false;
+            this.ButtonGenerarVxV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ButtonGenerarVxV.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonGenerarVxV.Location = new System.Drawing.Point(490, 83);
+            this.ButtonGenerarVxV.Name = "ButtonGenerarVxV";
+            this.ButtonGenerarVxV.Size = new System.Drawing.Size(133, 37);
+            this.ButtonGenerarVxV.TabIndex = 1;
+            this.ButtonGenerarVxV.Text = "Generar";
+            this.ButtonGenerarVxV.UseVisualStyleBackColor = false;
             // 
-            // ButtonVolver
+            // ButtonVolverVxV
             // 
-            this.ButtonVolver.BackColor = System.Drawing.Color.Blue;
-            this.ButtonVolver.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonVolver.ForeColor = System.Drawing.SystemColors.Control;
-            this.ButtonVolver.Location = new System.Drawing.Point(372, 76);
-            this.ButtonVolver.Name = "ButtonVolver";
-            this.ButtonVolver.Size = new System.Drawing.Size(133, 37);
-            this.ButtonVolver.TabIndex = 2;
-            this.ButtonVolver.Text = "Volver";
-            this.ButtonVolver.UseVisualStyleBackColor = false;
+            this.ButtonVolverVxV.BackColor = System.Drawing.Color.Blue;
+            this.ButtonVolverVxV.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonVolverVxV.ForeColor = System.Drawing.SystemColors.Control;
+            this.ButtonVolverVxV.Location = new System.Drawing.Point(624, 403);
+            this.ButtonVolverVxV.Name = "ButtonVolverVxV";
+            this.ButtonVolverVxV.Size = new System.Drawing.Size(133, 37);
+            this.ButtonVolverVxV.TabIndex = 2;
+            this.ButtonVolverVxV.Text = "Volver";
+            this.ButtonVolverVxV.UseVisualStyleBackColor = false;
             // 
             // cmbVentasxVendedor
             // 
             this.cmbVentasxVendedor.BackColor = System.Drawing.SystemColors.MenuBar;
             this.cmbVentasxVendedor.FormattingEnabled = true;
-            this.cmbVentasxVendedor.Location = new System.Drawing.Point(272, 119);
+            this.cmbVentasxVendedor.Location = new System.Drawing.Point(275, 91);
             this.cmbVentasxVendedor.Name = "cmbVentasxVendedor";
             this.cmbVentasxVendedor.Size = new System.Drawing.Size(195, 21);
             this.cmbVentasxVendedor.TabIndex = 3;
@@ -83,7 +83,7 @@
             // dgvVentarxVendor
             // 
             this.dgvVentarxVendor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvVentarxVendor.Location = new System.Drawing.Point(57, 159);
+            this.dgvVentarxVendor.Location = new System.Drawing.Point(58, 140);
             this.dgvVentarxVendor.Name = "dgvVentarxVendor";
             this.dgvVentarxVendor.Size = new System.Drawing.Size(661, 238);
             this.dgvVentarxVendor.TabIndex = 4;
@@ -95,10 +95,10 @@
             this.ClientSize = new System.Drawing.Size(784, 450);
             this.Controls.Add(this.dgvVentarxVendor);
             this.Controls.Add(this.cmbVentasxVendedor);
-            this.Controls.Add(this.ButtonVolver);
-            this.Controls.Add(this.ButtonGenerar);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.ButtonVolverVxV);
+            this.Controls.Add(this.ButtonGenerarVxV);
+            this.Controls.Add(this.labelVentasxVendedor);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormVentasPorVendedor";
             this.Text = "Ventas Por Vendedor";
             this.Load += new System.EventHandler(this.FormVentasPorVendedor_Load);
@@ -110,9 +110,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button ButtonGenerar;
-        private System.Windows.Forms.Button ButtonVolver;
+        private System.Windows.Forms.Label labelVentasxVendedor;
+        private System.Windows.Forms.Button ButtonGenerarVxV;
+        private System.Windows.Forms.Button ButtonVolverVxV;
         private System.Windows.Forms.ComboBox cmbVentasxVendedor;
         private System.Windows.Forms.DataGridView dgvVentarxVendor;
     }
